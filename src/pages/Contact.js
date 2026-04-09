f034aeaeb4a02346c73e780abb821c1fde9fb7eb
Sultan Hussain Barbhuiya <sultanbarbhuiya2004@gmail.com>
2026-04-09 20:48:25 +0530

    fixed contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,23 +7,23 @@ function Contact() {
       <div className="contact-item">WhatsApp: 1234567890</div>
 
       <div className="contact-item">
-        Email: <a href="#">sultan123@gmail.com</a>
+        Email: <a href="mailto:sultan123@gmail.com">sultan123@gmail.com</a>
       </div>
 
       <div className="contact-item">
-        Facebook: <a href="#">facebook.com/sultan</a>
+        Facebook: <a href="https://facebook.com" target="_blank" rel="noreferrer">facebook.com/sultan</a>
       </div>
 
       <div className="contact-item">
-        Instagram: <a href="#">instagram.com/sultan</a>
+        Instagram: <a href="https://instagram.com" target="_blank" rel="noreferrer">instagram.com/sultan</a>
       </div>
 
       <div className="contact-item">
-        Github: <a href="#">github.com/sultan</a>
+        Github: <a href="https://github.com" target="_blank" rel="noreferrer">github.com/sultan</a>
       </div>
 
       <div className="contact-item">
-        LinkedIn: <a href="#">linkedin.com/sultan</a>
+        LinkedIn: <a href="https://linkedin.com" target="_blank" rel="noreferrer">linkedin.com/sultan</a>
       </div>
 
       <div className="contact-item">
